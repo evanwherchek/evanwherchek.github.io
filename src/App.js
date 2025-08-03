49205caaad3f3f39e2bea23f274a61a695c96052
Evan Herchek <evanwherchek@gmail.com>
2025-08-03 11:13:11 -0500

    Improved sectioning

--- src/App.js
+++ src/App.js
@@ -1,39 +1,23 @@
 import Title from "./widgets/Title";
-import { motion } from "motion/react"
+import Bio from "./widgets/Bio";
 
 function App() {
   const styles = {
     backdrop: {
-      backgroundColor: '#101C2D',
-      width: '100%',
-      height: '100vh',
-      position: 'absolute',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
+        backgroundColor: '#101C2D',
+        width: '100%',
+        position: 'absolute',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
     }
   }
 
   return (
     <div style={styles.backdrop}>
-      <motion.div
-          initial={{ opacity: 0, y: 50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{
-            duration: 1,
-            delay: 0.5,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-      >
-        <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 3 }}
-        >
-          <Title />
-        </motion.p>
-      </motion.div>
+        <Title />
+        <Bio />
     </div>
   );
 }
--- src/widgets/Title.js
+++ src/widgets/Title.js
@@ -1,5 +1,6 @@
 import lindenSvg from '../assets/linden.svg';
 import oakSvg from '../assets/oak.svg';
+import { motion } from "motion/react"
 
 function Title() {
     const styles = {
@@ -27,13 +28,27 @@ function Title() {
     }
 
     return (
-        <div>
-            <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-                <img style={styles.leaf} src={lindenSvg} alt="Linden" />
-                <h1 style={styles.h1}>Evan Herchek</h1>
-                <img style={styles.leaf} src={oakSvg} alt="Oak" />
-            </div>
-            <h2 style={styles.h2}>Software Developer</h2>
+        <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <motion.div
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{
+                    duration: 1,
+                    delay: 0.5,
+                    ease: [0, 0.71, 0.2, 1.01],
+                }}>
+                <motion.p
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 3 }}>
+                    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                        <img style={styles.leaf} src={lindenSvg} alt="Linden" />
+                        <h1 style={styles.h1}>Evan Herchek</h1>
+                        <img style={styles.leaf} src={oakSvg} alt="Oak" />
+                    </div>
+                    <h2 style={styles.h2}>Software Developer</h2>
+                </motion.p>
+            </motion.div>
         </div>
     );
 }
