d25d222d623c9002085c159f9f93ea90109fdba4
Evan Herchek <evanwherchek@gmail.com>
2025-08-13 20:52:51 -0500

    Added background decoration

--- src/widgets/Title.js
+++ src/widgets/Title.js
@@ -6,25 +6,28 @@ function Title() {
     return (
         <div style={{height: '100vh', width: '100vw', display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#101C2D'}}>
             <Threads
-                amplitude={1}
+                amplitude={2}
                 distance={0}
-                enableMouseInteraction={true}
+                enableMouseInteraction={false}
             />
-            <motion.div
-                initial={{ opacity: 0, y: 50 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                    duration: 1,
-                    delay: 0.5,
-                    ease: [0, 0.71, 0.2, 1.01],
-                }}>
-                <motion.p
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 3 }}>
-                    <img src={logo} alt="logo" />
-                </motion.p>
-            </motion.div>
+            <div style={{position: 'absolute'}}>
+                <motion.div
+                    initial={{ opacity: 0, y: 50 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{
+                        duration: 1,
+                        delay: 0.5,
+                        ease: [0, 0.71, 0.2, 1.01],
+                    }}>
+                    <motion.p
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 3 }}>
+                        <img src={logo} alt="logo" />
+                    </motion.p>
+                </motion.div>
+            </div>
+
         </div>
     );
 }
