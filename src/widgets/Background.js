1ab880577b9800f9c976c2689210aa292ddf6672
Evan Herchek <evanwherchek@gmail.com>
2025-09-07 21:18:33 -0500

    Improved background section styling

--- src/widgets/Background.js
+++ src/widgets/Background.js
@@ -1,35 +1,26 @@
 import tamu from '../assets/tamu-logo.png';
 import nisc from '../assets/nisc-logo.png';
 import {Button} from "@mui/material";
-import GitHubIcon from "@mui/icons-material/GitHub";
 import {Download} from "@mui/icons-material";
+import StatText from "./custom/StatText";
 
 function Background() {
     return (
         <div className="h-screen w-full bg-showcase-grey flex flex-col items-center justify-center">
-            <h1 className="font-poly text-white font-thin text-5xl m-16 text-left underline">Background</h1>
-            <div className="flex flex-row items-center justify-center gap-[50px]">
-                <div>
-                    <p className="font-poly text-white font-thin text-7xl text-center">1 year</p>
-                    <p className="font-poly text-white font-thin text-2xl text-center w-40">of professional software engineering experience</p>
-                </div>
-                <div>
-                    <p className="font-poly text-white font-thin text-7xl text-center">6</p>
-                    <p className="font-poly text-white font-thin text-2xl text-center w-40">technology certifications</p>
-                </div>
-                <div>
-                    <p className="font-poly text-white font-thin text-7xl text-center">8 years</p>
-                    <p className="font-poly text-white font-thin text-2xl text-center w-40">of building software projects</p>
-                </div>
+            <h1 className="font-poly text-white font-thin text-5xl m-8 text-left underline">Background</h1>
+            <div className="flex flex-row align-top justify-center gap-12 m-8">
+                <StatText number={1} smallText={"year of full time software engineering experience"}/>
+                <StatText number={6} smallText={"cloud service provider certifications"}/>
+                <StatText number={8} smallText={"years of building software projects"}/>
             </div>
-            <div className="flex flex-row items-center justify-center gap-[50px]">
+            <div className="flex flex-row items-center justify-between gap-12 m-8">
                 <div>
                     <h2 className="font-poly text-white underline font-thin text-4xl text-center w-40">Education</h2>
                     <div className="flex flex-row items-center justify-center">
                         <img src={tamu} className="w-28" alt="Texas A&M Logo" />
                         <div>
                             <p className="font-poly text-white text-3xl">Texas A&M University</p>
-                            <p className="font-poly text-white text-2xl">BA in Computing(Computer Science)</p>
+                            <p className="font-poly text-white text-2xl">BA in Computing</p>
                             <p className="font-poly text-white text-2xl">2021 - 2024, GPA: 3.3</p>
                         </div>
                     </div>
@@ -39,7 +30,7 @@ function Background() {
                     <div className="flex flex-row items-center justify-center">
                         <img src={nisc} className="w-28" alt="NISC Logo" />
                         <div>
-                            <p className="font-poly text-white text-3xl">National Information Solutions Cooperative</p>
+                            <p className="font-poly text-white text-3xl">NISC</p>
                             <p className="font-poly text-white text-2xl">Software Developer</p>
                             <p className="font-poly text-white text-2xl">2024 - Present</p>
                         </div>
