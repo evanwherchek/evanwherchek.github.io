4fb4e005ad7ca4a643b5cae3bd7e33aede39e7fd
Evan Herchek <evanwherchek@gmail.com>
2025-09-17 21:39:56 -0500

    Improved contact section styling

--- src/widgets/Contact.js
+++ src/widgets/Contact.js
@@ -4,7 +4,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 function Contact() {
     return (
-        <div className="h-screen w-full flex flex-col bg-black items-center justify-center">
+        <div className="w-full pt-52 pb-52 flex flex-col bg-gray-800 items-center justify-center">
             <h1 className="font-poly text-white font-thin text-xl mt-0 text-left m-1">Building something cool? Let's talk about it!</h1>
             <p className="font-poly text-white font-thin text-lg mt-0 text-left m-1">hello@evanherchek.dev</p>
             <div className="flex flex-row items-center justify-between gap-8 m-1">
