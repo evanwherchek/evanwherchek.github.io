db513a56c5bf399e2cd1515e58845e4c67badbeb
Evan Herchek <evanwherchek@gmail.com>
2025-09-21 16:54:18 -0500

    Added untracked files

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{\"/_app\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/208aed4ce26a1da1-s.p.woff2\"]},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":true}"
